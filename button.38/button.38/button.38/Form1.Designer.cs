c7dfb529bbba86397906d5420e29be81db837214
11meryem <145452149+11meryem@users.noreply.github.com>
2023-11-20 10:43:58 +0300

    Add files via upload

---
+++ button.38/button.38/button.38/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace button._38
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtAra = new System.Windows.Forms.TextBox();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "ayse",
+            "mehmet",
+            "kadriye",
+            "kadir",
+            "gökçe",
+            "büşra",
+            "meryem",
+            "mustafa",
+            "muhammet",
+            "burcu",
+            "sude",
+            "seyma",
+            "ömer"});
+            this.listBox1.Location = new System.Drawing.Point(166, 53);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(108, 212);
+            this.listBox1.TabIndex = 0;
+            // 
+            // txtAra
+            // 
+            this.txtAra.Location = new System.Drawing.Point(32, 109);
+            this.txtAra.Name = "txtAra";
+            this.txtAra.Size = new System.Drawing.Size(100, 20);
+            this.txtAra.TabIndex = 2;
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(32, 63);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(75, 23);
+            this.btnAra.TabIndex = 3;
+            this.btnAra.Text = "button1";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 436);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.txtAra);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtAra;
+        private System.Windows.Forms.Button btnAra;
+    }
+}
+
